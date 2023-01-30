b93d6aaf39060fc436ed10460e9a23a3bb48abe3
Robert Brown <robert.brown@contentcoffee.com>
2023-01-30 18:50:31 +0100

    cleaning up search results

--- resources/views/notice/index.blade.php
+++ resources/views/notice/index.blade.php
@@ -17,30 +17,7 @@
 
     <h1 class="ecl-page-header__title">Notices</h1>
 
-    <table class="ecl-table ecl-table--zebra">
-        <thead class="ecl-table__head">
-        <tr class="ecl-table__row">
-            <th class="ecl-table__header">Title</th>
-            <th class="ecl-table__header">Entities</th>
-            <th class="ecl-table__header"></th>
-            <th class="ecl-table__header"></th>
-            <th class="ecl-table__header"></th>
-        </tr>
-        </thead>
-        <tbody class="ecl-table__body">
-        @foreach($notices as $notice)
-            <tr class="ecl-table__row">
-                <td class="ecl-table__cell"><a class="ecl-link" href="{{ route('notice.show', [$notice]) }}">{{ $notice->title }}</a></td>
-                <td class="ecl-table__cell">{{ implode(', ', $notice->entities()->pluck('name')->toArray()) }}</td>
-                <td class="ecl-table__cell"></td>
-                <td class="ecl-table__cell"></td>
-                <td class="ecl-table__cell"></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
-    {{ $notices->links('paginator') }}
+    <x-notices-table :notices=$notices />
 
 @endsection
 
