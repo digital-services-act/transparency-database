9e2c71a1a404475f5136113bc2e09337094b68d7
Robert Brown <robert.brown@contentcoffee.com>
2023-02-01 12:21:08 +0100

    details...

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,7 +5,7 @@
 
 @section('breadcrumbs')
     <x-ecl.breadcrumb label="Home" url="{{ route('home') }}"/>
-    <x-ecl.breadcrumb label="Profile Dashboard" />
+    <x-ecl.breadcrumb label="{{ $page_title }}" />
 @endsection
 
 
@@ -47,7 +47,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/monokai-sublime.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
-    
+
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 
     <script>
