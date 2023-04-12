a3d456ff85151f3244a8c95798ddc61c3509f452
Robert Brown <robert.brown@contentcoffee.com>
2023-04-12 09:25:42 +0200

    get the more advance search started

--- resources/views/statement/index.blade.php
+++ resources/views/statement/index.blade.php
@@ -12,6 +12,21 @@
     <div class="ecl-u-mt-l ecl-u-mb-l ecl-u-f-r">
         <form method="get">
             <x-ecl.textfield name="s" label="Search <a class='ecl-link' href='{{ route('statement.index') }}'>clear</a>" placeholder="search by name and uuid" justlabel="true" value="{{ request()->get('s', '') }}" />
+            <div class="ecl-expandable" data-ecl-expandable="true" data-ecl-auto-init="Expandable"><button
+                        class="ecl-button ecl-button--secondary ecl-expandable__toggle" type="button"
+                        aria-controls="expandable-example-content" data-ecl-expandable-toggle="" data-ecl-label-expanded="Expanded"
+                        data-ecl-label-collapsed="Collapsed" aria-expanded="false"><span class="ecl-button__container"><span
+                                class="ecl-button__label" data-ecl-label="true">Collapsed</span><svg
+                                class="ecl-icon ecl-icon--fluid ecl-icon--rotate-180 ecl-button__icon ecl-button__icon--after" focusable="false"
+                                aria-hidden="true" data-ecl-icon="">
+        <use xlink:href="/component-library/dist/media/icons.eae40d34.svg#corner-arrow"></use>
+      </svg></span></button>
+                <div id="expandable-example-content" class="ecl-expandable__content" hidden="">
+                    <p class="ecl-u-type-paragraph-m">The EU is building an energy union that make sures Europe’s energy supply is safe,
+                        viable and accessible to all. In doing so, it can boost the economy and attract investments that can create new
+                        jobs opportunities.</p>
+                </div>
+            </div>
         </form>
     </div>
 
