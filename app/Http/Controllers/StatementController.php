c3c217b504d2f0cd6e6ac4b00a4e6ddbb862ff50
Robert Brown <robert.brown@contentcoffee.com>
2023-05-02 14:29:21 +0200

    abstraction

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -40,35 +40,43 @@ class StatementController extends Controller
 
         $statements = $statements->orderBy('created_at', 'DESC')->paginate(50)->withQueryString();
 
-
-        $similarity_results = false;
+        $similarity_results = null;
         if ($request->get('s')) {
-
-            // Actual Request
-            $payload = new stdClass();
-            $payload->service = 'similarity';
-            $payload->text = $request->get('s');
-            $payload->parameters = new stdClass();
-            $payload->parameters->lang = 'en';
-
-            $payload = json_encode($payload);
-
-            $headers = [];
-            $headers['x-api-key'] = config('services.drivein.key');
-            $headers['Accept'] = 'application/json';
-
-            $response = Http::withHeaders($headers)
-                            ->withBody($payload, 'application/json')
-                            ->post(config('services.drivein.base'));
-
-            $similarity_results = $response->json('result');
-            $similarity_results = array_map(function($item){
-                return str_replace("_", " ", $item);
-            }, $similarity_results);
+            $similarity_results = $this->getSimilarityWords($request->get('s'));
         }
 
-
         return view('statement.index', compact('statements', 'options', 'total', 'similarity_results'));
+    }
+
+    /**
+     * @param $word
+     *
+     * @return array
+     */
+    private function getSimilarityWords($word): array
+    {
+        // Actual Request
+        $payload = new stdClass();
+        $payload->service = 'similarity';
+        $payload->text = $word;
+        $payload->parameters = new stdClass();
+        $payload->parameters->lang = 'en';
+
+        $payload = json_encode($payload);
+
+        $headers = [];
+        $headers['x-api-key'] = config('services.drivein.key');
+        $headers['Accept'] = 'application/json';
+
+        $response = Http::withHeaders($headers)
+                        ->withBody($payload, 'application/json')
+                        ->post(config('services.drivein.base'));
+
+        $similarity_results = $response->json('result');
+
+        return array_map(function($item){
+            return str_replace("_", " ", $item);
+        }, $similarity_results);
     }
 
     /**
