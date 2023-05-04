831e2edef38fb7e6d7a235e0bfc0dc8a6d8d18e7
Robert Brown <robert.brown@contentcoffee.com>
2023-05-04 10:09:22 +0200

    Add it to the migration

--- database/migrations/2023_01_13_082758_create_statements_table.php
+++ database/migrations/2023_01_13_082758_create_statements_table.php
@@ -20,6 +20,7 @@ class CreateStatementsTable extends Migration
 
             $table->enum('decision_taken', array_keys(\App\Models\Statement::DECISIONS));
             $table->enum('decision_ground', array_keys(\App\Models\Statement::DECISION_GROUNDS));
+            $table->enum('category', array_keys(\App\Models\Statement::SOR_CATEGORIES));
 
 
             $table->string('illegal_content_legal_ground', 255)->nullable();
