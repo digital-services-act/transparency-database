c7a5d34c430a7b9cf114aae92357cb3e3422259b
Robert Brown <robert.brown@contentcoffee.com>
2023-06-08 16:58:09 +0200

    oops

--- resources/views/statement/index.blade.php
+++ resources/views/statement/index.blade.php
@@ -15,11 +15,11 @@
 
     <h1 class="ecl-page-header__title ecl-u-type-heading-1 ecl-u-mb-l">Statements</h1>
 
-{{--    @can('create statements')--}}
+    @can('create statements')
         <p class="ecl-u-type-paragraph"></p>
         <x-ecl.cta-button label="Create a Statement" url="{{ route('statement.create') }}"/>
         </p>
-{{--    @endcan--}}
+    @endcan
 
     <p class="ecl-u-type-paragraph">Statements Found: {{ $total }}</p>
     <x-statements-table :statements="$statements" />
