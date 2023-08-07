bb191271379c6e9a958dd3b273b90cd8145b7cb9
Alain Van Driessche <alainvd@gmail.com>
2023-08-07 15:02:12 +0200

    Platform name

--- resources/views/statement/show.blade.php
+++ resources/views/statement/show.blade.php
@@ -14,7 +14,7 @@
     <h1 class="ecl-page-header__title ecl-u-type-heading-1 ecl-u-mb-l">Statement Details: {{$statement->uuid}}</h1>
 
     @if($statement->user && $statement->user->platform)
-        <x-infoline title="Sent by" :content="$statement->user->platform->name"></x-infoline>
+        <x-infoline title="Platform name" :content="$statement->user->platform->name"></x-infoline>
     @endif
 
     <x-infoline title="Received" :content="$statement->created_at->format('Y-m-d H:i:s')"></x-infoline>
