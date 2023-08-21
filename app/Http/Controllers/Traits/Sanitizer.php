5400a1fad5c0dee3ffa993f790ed637ae7507971
Alain Van Driessche <alainvd@gmail.com>
2023-08-21 10:05:29 +0200

    remove end_date

--- app/Http/Controllers/Traits/Sanitizer.php
+++ app/Http/Controllers/Traits/Sanitizer.php
@@ -14,7 +14,6 @@ trait Sanitizer
     {
         $validated['application_date'] = $this->sanitizeDate($validated['application_date'] ?? null);
         $validated['content_date'] = $this->sanitizeDate($validated['content_date'] ?? null);
-        $validated['end_date'] = $this->sanitizeDate($validated['end_date'] ?? null);
         $validated['end_date_monetary_restriction'] = $this->sanitizeDate($validated['end_date_monetary_restriction'] ?? null);
         $validated['end_date_visibility_restriction'] = $this->sanitizeDate($validated['end_date_visibility_restriction'] ?? null);
         $validated['end_date_account_restriction'] = $this->sanitizeDate($validated['end_date_account_restriction'] ?? null);
