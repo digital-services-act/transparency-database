af32a62c0a4caa53adc47be4ae515225b84e9392
Robert Brown <robert.brown@contentcoffee.com>
2023-08-24 14:30:55 +0200

    carry over the query params from the search...

--- resources/views/statement/index.blade.php
+++ resources/views/statement/index.blade.php
@@ -31,7 +31,7 @@
 
         <div class="ecl-u-type-paragraph ecl-u-mr-l">
 
-            <a href="{{ route('statement.export') }}" class="ecl-link ecl-link--default ecl-link--icon ecl-link--icon-after">
+            <a href="{{ route('statement.export', request()->query()) }}" class="ecl-link ecl-link--default ecl-link--icon ecl-link--icon-after">
                     <span class="ecl-link__label">.csv</span>
                     <svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true">
                         <x-ecl.icon icon="download" />
