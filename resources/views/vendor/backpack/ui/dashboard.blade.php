e51e51c32fc0b1b97a0f84034f6d6093851d17b3
Alain Van Driessche <alainvd@gmail.com>
2023-08-25 17:08:20 +0200

    Some charts

--- resources/views/vendor/backpack/ui/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -1,24 +1,72 @@
 @extends(backpack_view('blank'))
 
 @php
-    if (backpack_theme_config('show_getting_started')) {
-        $widgets['before_content'][] = [
-            'type'        => 'view',
-            'view'        => backpack_view('inc.getting_started'),
-        ];
-    } else {
 
+$widgets['before_content'][] =
+[
+'type'    => 'div',
+'class'   => 'row',
+'content' =>
+    [ // widgets here
+        [
+        'type'       => 'chart',
+        'controller' => \App\Http\Controllers\Admin\Charts\TopPlatformsChartController::class,
 
-        $widgets['before_content'][] = [
-    'type'         => 'alert',
-    'class'        => 'alert alert-danger mb-2',
-    'heading'      => 'Important information!',
-    'content'      => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti nulla quas distinctio veritatis provident mollitia error fuga quis repellat, modi minima corporis similique, quaerat minus rerum dolorem asperiores, odit magnam.',
-    'close_button' => true, // show close button or not
+        // OPTIONALS
+
+        'class'   => 'card mb-2',
+        'wrapper' => ['class'=> 'col-md-6'] ,
+        'content' => [
+              'header' => 'Top platforms',
+              'body'   => 'This chart shows the 5 most active platforms.<br><br>',
+         ],
+        ],
+        [
+        'type'       => 'chart',
+        'controller' => \App\Http\Controllers\Admin\Charts\DailyStatementsChartController::class,
+
+        // OPTIONALS
+
+        'class'   => 'card mb-2',
+        'wrapper' => ['class'=> 'col-md-6'] ,
+        'content' => [
+              'header' => 'New Statements',
+              'body'   => 'This chart should make it obvious how many new statements have been created in the past 7 days.<br><br>',
+         ],
+        ]
+
+    ]
 ];
-    }
+
+$widgets['before_content'][] =
+[
+'type'    => 'div',
+'class'   => 'row',
+'content' =>
+    [ // widgets here
+        [
+        'type'       => 'chart',
+        'controller' => \App\Http\Controllers\Admin\Charts\TopCategoriesChartController::class,
+
+        // OPTIONALS
+
+        'class'   => 'card mb-2',
+        'wrapper' => ['class'=> 'col-md-12'] ,
+        'content' => [
+              'header' => 'Top categories',
+              'body'   => 'This chart shows the most popular categories.<br><br>',
+         ],
+        ]
+
+
+    ]
+]
+
+
+
+
 @endphp
 
 @section('content')
-    Welcome to the DSA Admin Panel
+    {{--    <h2>Welcome to the DSA Admin Panel</h2>--}}
 @endsection
