875ae5134c11bb2149398550baddeb4f2b89b10e
Robert Brown <robert.brown@contentcoffee.com>
2023-08-25 09:38:24 +0200

    allowing the checkboxes flex to put a width on each flex cell.

--- resources/views/components/ecl/checkboxes-flex.blade.php
+++ resources/views/components/ecl/checkboxes-flex.blade.php
@@ -1,4 +1,4 @@
-@props(['label' => 'label', 'id' => 'id', 'name' => 'name', 'required' => false, 'help' => null, 'options' => [], 'default' => [], 'justlabel' => false])
+@props(['label' => 'label', 'id' => 'id', 'name' => 'name', 'required' => false, 'help' => null, 'options' => [], 'default' => [], 'justlabel' => false, 'width' => false])
 
 <fieldset class="ecl-form-group ecl-u-mb-l">
 
@@ -11,7 +11,7 @@
     <div class="demo-container ecl-u-d-flex ecl-u-flex-wrap ecl-u-mt-m">
         @if(is_array($options) && is_array($default))
             @foreach($options as $option)
-                <div class="ecl-u-mr-m">
+                <div class="ecl-u-mr-m" @if($width)style="width: {{$width}}; @endif">
                     <x-ecl.checkbox id="{{ $id }}-{{ $loop->iteration }}" label="{{ $option['label'] }}" name="{{ $name }}[]" value="{{ $option['value'] }}" :checked="in_array($option['value'], $default)" />
                 </div>
             @endforeach
