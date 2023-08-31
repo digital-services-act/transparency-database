ff5e9e304e4a619fa41326b5f0a2f111a5c7c642
Robert Brown <robert.brown@contentcoffee.com>
2023-08-31 11:04:18 +0200

    do it withouth jquery

--- resources/views/components/statement/table.blade.php
+++ resources/views/components/statement/table.blade.php
@@ -1,5 +1,13 @@
 @php use App\Models\Statement; @endphp
 @props(['statements' => null])
+
+<style>
+    .statement-row:hover {
+        background-color: #EBEBEB !important;
+        cursor: pointer !important;
+    }
+</style>
+
 <table class="ecl-table ecl-table--zebra">
     <thead class="ecl-table__head">
     <tr class="ecl-table__row">
@@ -7,28 +15,34 @@
         <th class="ecl-table__header">Restrictions</th>
         <th class="ecl-table__header">Category</th>
         <th class="ecl-table__header">Creation Date</th>
-        <th class="ecl-table__header">&nbsp;</th>
+
 
     </tr>
     </thead>
     <tbody class="ecl-table__body">
     @foreach($statements as $statement)
-        <tr class="ecl-table__row">
+
+        <tr class="ecl-table__row statement-row" data-url="{{ route('statement.show', [$statement]) }}">
             <td class="ecl-table__cell">{{$statement->platform_name}}</td>
             <td class="ecl-table__cell">{{$statement->restrictions()}}</td>
             <td class="ecl-table__cell">{{Statement::STATEMENT_CATEGORIES[$statement->category]}}</td>
             <td class="ecl-table__cell">{{ $statement->created_at->format('Y-m-d') }}</td>
-            <td class="ecl-table__cell">
-                <a class="ecl-link ecl-link--default ecl-link--icon ecl-link--icon-after"
-                   href="{{ route('statement.show', [$statement]) }}">
-                    <svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true">
-                        <x-ecl.icon icon="file" />
-                    </svg>
-                </a>
-            </td>
         </tr>
+
     @endforeach
     </tbody>
 </table>
+
+
+<script>
+  document.addEventListener('DOMContentLoaded', (event) => {
+    let rows = document.getElementsByClassName('statement-row');
+    for (let i = 0; i < rows.length; i++) {
+      rows[i].addEventListener('click', (e) => {
+        document.location.href = rows[i].getAttribute('data-url');
+      });
+    }
+  });
+</script>
 
 {{ $statements->links('paginator') }}
