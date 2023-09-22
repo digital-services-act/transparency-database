f23bb7ba26aa936c79a76ec76b1e9d6e143e302a
Robert Brown <robert.brown@contentcoffee.com>
2023-09-22 12:58:36 +0200

    do a db query if we are not in opensearch.

--- app/Services/PlatformDayTotalsService.php
+++ app/Services/PlatformDayTotalsService.php
@@ -100,15 +100,46 @@ class PlatformDayTotalsService
         if ($existing !== false) {
             return $existing;
         }
+
         if (config('scout.driver') != 'opensearch') {
+
+            $start = $date->clone();
+            $end = $date->clone();
+
+            $start->hour = 0;
+            $start->minute = 0;
+            $start->second = 0;
+
+            $end->hour = 23;
+            $end->minute = 59;
+            $end->second = 59;
+
+            // CRITICAL OBSERVATION
+            // This is the whole potato of doing the stats and analytics here.
+            // If this is slow, then we need to drop down to a raw query
+            // If a raw query is slow then it's game over.
+            $query = Statement::query()->where('platform_id', $platform->id)->where('created_at', '>=', $start)->where('created_at', '<=', $end);
+
+            if ($attribute !== '*' && $value !== '*') {
+                $query->where($attribute, 'LIKE', '%'.$value.'%');
+            }
+
+            if ($attribute !== '*' && $value === '*') {
+                $query->whereNotNull($attribute);
+            }
+
+            $total = $query->count(); // <- this guy right here is the Achilles' heel
+            // END OBSERVATION
+
             PlatformDayTotal::create([
                 'date' => $date,
                 'platform_id' => $platform->id,
                 'attribute' => $attribute,
                 'value' => $value,
-                'total' => rand(0, 1000)
+                'total' => $total
             ]);
-            Log::warning('Compiling a day total requires opensearch, using a random number.');
+
+            return $total;
         }
 
         $s = $date->format('Y-m-d') . 'T00:00:00';
