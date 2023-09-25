764671d9ea57ca2b88124353fec2ff17391006f9
Robert Brown <robert.brown@contentcoffee.com>
2023-09-25 14:20:58 +0200

    tweaks

--- resources/views/statement/show.blade.php
+++ resources/views/statement/show.blade.php
@@ -17,7 +17,7 @@
         <x-infoline title="Platform name" :content="$statement->user->platform->name"></x-infoline>
     @endif
 
-    <x-infoline title="Received" :content="$statement->created_at->format('Y-m-d H:i:s')"></x-infoline>
+    <x-infoline title="Received" :content="$statement->created_at->format('Y-m-d H:i:s')"> UTC</x-infoline>
 
     @if(!is_null($statement->decision_visibility))
         <x-infoline :title="Statement::LABEL_STATEMENT_DECISION_VISIBILITY"
