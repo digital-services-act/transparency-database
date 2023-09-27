94e941325c1eb234cd36691a9668cd5f33dc4652
Robert Brown <robert.brown@contentcoffee.com>
2023-09-27 09:47:39 +0200

    finally some good responsive clean up going on here for the simple form.

---
+++ resources/views/components/ecl/search.blade.php
@@ -0,0 +1,21 @@
+@props(['label' => 'Search', 'name' => 'name', 'id' => 'id', 'size' => 'm', 'placeholder' => '', 'value' => null])
+
+<div class="ecl-form-group">
+    <label for="{{ $id }}" class="ecl-form-label ecl-search-form__label">{{ $label }}</label>
+    <input type="search"
+           class="ecl-text-input ecl-text-input--{{ $size }} ecl-search-form__text-input"
+           name="{{ $name }}"
+           id="{{ $id }}"
+           placeholder="{{ $placeholder }}"
+           value="{{ $value }}"
+    />
+</div>
+<button class="ecl-button ecl-button--search ecl-search-form__button" type="submit" aria-label="{{ $label }}">
+    <span class="ecl-button__container">
+        <span class="ecl-button__label" data-ecl-label="true">{{ $label }}</span>
+        <svg class="ecl-icon ecl-icon--xs ecl-button__icon ecl-button__icon--after"
+             focusable="false"
+             aria-hidden="true"
+             data-ecl-icon=""><x-ecl.icon icon="search"/></svg>
+    </span>
+</button>
