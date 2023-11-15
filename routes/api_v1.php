c2609e7252ef5a5d3b7309fdf44390cd509ab525
Robert Brown <robert.brown@contentcoffee.com>
2023-11-15 11:10:01 +0100

    expose the opensearch api with a simple passthrough endpoint only to admins.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\SearchAPIController;
 use App\Http\Controllers\Api\v1\StatementAPIController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,9 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('statement/existing-puid/{puid}', [StatementAPIController::class,'existingPuid'])->name('api.v1.statement.existing-puid')->can('view statements');
     Route::post('statement', [StatementAPIController::class,'store'])->name('api.v1.statement.store')->can('create statements');
     Route::post('statements', [StatementAPIController::class,'storeMultiple'])->name('api.v1.statements.store')->can('create statements');
+
+    Route::group(['middleware' => ['can:administrate']], static function(){
+        Route::post('search', [SearchAPIController::class, 'passThrough'])->name('api.v1.search');
+    });
 });
 
