766623143b8ec91ddc401b0b1e15f8b2f82e86c9
Robert Brown <robert.brown@contentcoffee.com>
2023-11-28 20:47:02 +0100

    hiding the csv file features.

--- resources/views/components/dayarchive/table.blade.php
+++ resources/views/components/dayarchive/table.blade.php
@@ -42,12 +42,12 @@
                     <span class="ecl-link__label">zip</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>
                 </a>
                 &nbsp;&nbsp;&nbsp;
-                <a href="{{ $dayarchive->sha1url }}" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after">
-                    <span class="ecl-link__label">sha1</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>
-                </a>
+{{--                <a href="{{ $dayarchive->sha1url }}" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after">--}}
+{{--                    <span class="ecl-link__label">sha1</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>--}}
+{{--                </a>--}}
             </td>
             <td class="ecl-table__cell" data-ecl-table-header="Size">
-                {{human_filesize($dayarchive->size)}}&nbsp;.csv<br/>
+{{--                {{human_filesize($dayarchive->size)}}&nbsp;.csv<br/>--}}
                 {{human_filesize($dayarchive->zipsize)}}&nbsp;.zip
             </td>
             <td class="ecl-table__cell" data-ecl-table-header="File">
@@ -56,12 +56,12 @@
                     <span class="ecl-link__label">zip</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>
                 </a>
                 &nbsp;&nbsp;&nbsp;
-                <a href="{{ $dayarchive->sha1urllight }}" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after">
-                    <span class="ecl-link__label">sha1</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>
-                </a>
+{{--                <a href="{{ $dayarchive->sha1urllight }}" class="ecl-link ecl-link--standalone ecl-link--icon ecl-link--icon-after">--}}
+{{--                    <span class="ecl-link__label">sha1</span><svg class="ecl-icon ecl-icon--fluid ecl-link__icon" focusable="false" aria-hidden="true"><x-ecl.icon icon="download"/></svg>--}}
+{{--                </a>--}}
             </td>
             <td class="ecl-table__cell" data-ecl-table-header="Size">
-                {{human_filesize($dayarchive->sizelight)}}&nbsp;.csv<br />
+{{--                {{human_filesize($dayarchive->sizelight)}}&nbsp;.csv<br />--}}
                 {{human_filesize($dayarchive->ziplightsize)}}&nbsp;.zip
             </td>
         </tr>
