57475b8f780a187394cbf3c65fdf5abeb954f42b
Robert Brown <robert.brown@contentcoffee.com>
2023-11-30 07:55:20 +0100

    order must be there.

--- app/Services/DayArchiveService.php
+++ app/Services/DayArchiveService.php
@@ -174,14 +174,14 @@ class DayArchiveService
                                     ->selectRaw($select_raw)
                                     ->where('statements.created_at', '>=', $date->format('Y-m-d') . ' 00:00:00')
                                     ->where('statements.created_at', '<=', $date->format('Y-m-d') . ' 23:59:59');
-                                    //->orderBy('statements.id', 'desc');
+                                    ->orderBy('statements.id', 'desc');
 
         } else {
             $raw                = DB::table('statements')
                                     ->selectRaw($select_raw)
                                     ->where('statements.id', '>=', $first_id)
                                     ->where('statements.id', '<=', $last_id);
-                                    //->orderBy('statements.id');
+                                    ->orderBy('statements.id');
         }
 
         return $raw;
