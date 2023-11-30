4a7fb6513defa9a2e3ac1124d52be8d66e15ccfd
Robert Brown <robert.brown@contentcoffee.com>
2023-11-30 15:12:04 +0100

    making a command and option to refresh all day totals.

--- app/Jobs/CompilePlatformDayTotal.php
+++ app/Jobs/CompilePlatformDayTotal.php
@@ -19,16 +19,18 @@ class CompilePlatformDayTotal implements ShouldQueue
     public string $date; // Y-m-d
     public string $attribute;
     public string $value;
+    public bool $delete_existing;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($platform_id, $date, $attribute = '*', $value = '*')
+    public function __construct($platform_id, $date, $attribute = '*', $value = '*', $delete_existing = false)
     {
         $this->platform_id = $platform_id;
         $this->date = $date;
         $this->attribute = $attribute;
         $this->value = $value;
+        $this->delete_existing = $delete_existing;
     }
 
     /**
@@ -41,7 +43,7 @@ class CompilePlatformDayTotal implements ShouldQueue
         $date = Carbon::createFromFormat('Y-m-d', $this->date);
 
         if ($platform && $date) {
-            $platform_day_totals_service->compileDayTotal($platform, $date, $this->attribute, $this->value);
+            $platform_day_totals_service->compileDayTotal($platform, $date, $this->attribute, $this->value, $this->delete_existing);
         }
     }
 }
