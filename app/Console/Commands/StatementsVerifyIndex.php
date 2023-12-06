990bd47ffd53909483e94835e59c8c2957d2fabf
Robert Brown <robert.brown@contentcoffee.com>
2023-12-06 08:30:09 +0100

    () are not needed.

--- app/Console/Commands/StatementsVerifyIndex.php
+++ app/Console/Commands/StatementsVerifyIndex.php
@@ -32,7 +32,7 @@ class StatementsVerifyIndex extends Command
         $chunk = 1000000;
 
         $min = $this->argument('min') === 'default' ? DB::table('statements')->selectRaw('MIN(id) AS min')->first()->min : (int)$this->argument('min');
-        $max = $this->argument('max') === 'default' ? (DB::table('statements')->selectRaw('MAX(id) AS max')->first()->max) : (int)$this->argument('max');
+        $max = $this->argument('max') === 'default' ? DB::table('statements')->selectRaw('MAX(id) AS max')->first()->max : (int)$this->argument('max');
 
         VerifyIndex::dispatch($max, $chunk, $min);
     }
