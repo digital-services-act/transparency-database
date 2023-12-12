9e3bd69be485a83bb7a454b5023330c9d5f42f9a
Robert Brown <robert.brown@contentcoffee.com>
2023-12-12 12:17:58 +0100

    use the cache but not rate limiting.

--- app/Jobs/StatementIndexRange.php
+++ app/Jobs/StatementIndexRange.php
@@ -48,7 +48,7 @@ class StatementIndexRange implements ShouldQueue
     public function handle(Client $client): void
     {
         // Set this in cache, to emergency stop reindexing.
-        $stop = false;//Cache::get('stop_reindexing', false);
+        $stop = Cache::get('stop_reindexing', false);
 
         if (!$stop) {
 
