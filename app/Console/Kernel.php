e7eafac8fdfa36f06df6f53d1c07548d8a03ee8e
Robert Brown <robert.brown@contentcoffee.com>
2023-12-13 13:43:08 +0100

    Refactor daily scheduling logic in Kernel class
    Attrition of various daily scheduling commands has been abstracted into a new private method `scheduleCommandsToRunDaily()`. This eliminates redundancy and improves code readability in the Kernel class, making future maintenance and feature addition more straightforward.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,56 +7,31 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param Schedule $schedule
-     * @return void
-     */
+    private const DAILY_3AM = '03:00';
+
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('platform:compile-day-totals')->dailyAt('03:00');
-        $schedule->command('platform:compile-day-totals all decision_ground DECISION_GROUND_ILLEGAL_CONTENT')->dailyAt('03:00');
-        $schedule->command('platform:compile-day-totals all decision_ground DECISION_GROUND_INCOMPATIBLE_CONTENT')->dailyAt('03:00');
-        $schedule->command('platform:compile-day-totals-categories')->dailyAt('03:00');
-        $schedule->command('platform:compile-day-totals-keywords')->dailyAt('03:00');
-        $schedule->command('platform:compile-day-totals-decisions')->dailyAt('03:00');
-
+        $this->scheduleCommandsToRunDaily($schedule);
         $schedule->command('statements:index-last-x')->everyFiveMinutes();
 
         // These ones run on a separate machine long running process.
         //$schedule->command('statements:day-archive')->daily();
-
-        /*
-
-          Put any typical commands that need to be run after dev/local reset-application here so that a dev can have first initial db..
-
-            php artisan statements:optimize-index &&
-            php artisan platform:compile-day-totals &&
-            php artisan platform:compile-day-totals all decision_ground DECISION_GROUND_ILLEGAL_CONTENT &&
-            php artisan platform:compile-day-totals all decision_ground DECISION_GROUND_INCOMPATIBLE_CONTENT &&
-            php artisan platform:compile-day-totals-categories &&
-            php artisan platform:compile-day-totals-keywords &&
-            php artisan platform:compile-day-totals-decisions &&
-
-            php artisan statements:day-archive
-            php artisan statements:day-archive 2023-10-01
-            php artisan statements:day-archive 2023-10-02
-            php artisan statements:day-archive 2023-10-03
-            ...
-
-        */
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    private function scheduleCommandsToRunDaily(Schedule $schedule): void
     {
-        $this->load(__DIR__.'/Commands');
+        $schedule->command('platform:compile-day-totals')->dailyAt(self::DAILY_3AM);
+        $schedule->command('platform:compile-day-totals all decision_ground DECISION_GROUND_ILLEGAL_CONTENT')->dailyAt(self::DAILY_3AM);
+        $schedule->command('platform:compile-day-totals all decision_ground DECISION_GROUND_INCOMPATIBLE_CONTENT')->dailyAt(self::DAILY_3AM);
+        $schedule->command('platform:compile-day-totals-categories')->dailyAt(self::DAILY_3AM);
+        $schedule->command('platform:compile-day-totals-keywords')->dailyAt(self::DAILY_3AM);
+        $schedule->command('platform:compile-day-totals-decisions')->dailyAt(self::DAILY_3AM);
+    }
 
+    // Existing `commands` method remains unchanged.
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
         require base_path('routes/console.php');
     }
 }
