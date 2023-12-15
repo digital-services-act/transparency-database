33f822c818c8729e79468daf1360ed8ad652d1e2
Robert Brown <robert.brown@contentcoffee.com>
2023-12-15 20:21:50 +0100

    Optimize StatementsVerifyIndexDate with fallback logic
    The commit refactors the function handle in StatementsVerifyIndexDate command, adding a fallback logic to handle cases when the first attempt to obtain the highest or lowest ID for the day fails. The updated code also streamlines caching and job dispatching procedures, improving the maintainability of the code.

--- app/Console/Commands/StatementsVerifyIndexDate.php
+++ app/Console/Commands/StatementsVerifyIndexDate.php
@@ -31,28 +31,28 @@ class StatementsVerifyIndexDate extends Command
      */
     public function handle(DayArchiveService $day_archive_service): void
     {
-        $query_chunk = $this->argument('query_chunk') === 'default' ? 100000 : (int)$this->argument('query_chunk');
+        $query_chunk      = $this->argument('query_chunk') === 'default' ? 100000 : (int)$this->argument('query_chunk');
         $searchable_chunk = $this->argument('searchable_chunk') === 'default' ? 1000 : (int)$this->argument('searchable_chunk');
-        $date = $this->argument('date') === 'default' ? Carbon::yesterday() : Carbon::createFromFormat('Y-m-d', $this->argument('date'));
+        $date             = $this->argument('date') === 'default' ? Carbon::yesterday() : Carbon::createFromFormat('Y-m-d', $this->argument('date'));
 
         $min = $day_archive_service->getFirstIdOfDate($date);
         $max = $day_archive_service->getLastIdOfDate($date);
 
         if ($min && $max) {
             Log::info('Index verification started for date: ' . $date->format('Y-m-d') . ' :: ' . $max . ' :: ' . $min);
-            Cache::forever('verify_jobs', 1);
-            Cache::forever('verify_jobs_run', 1);
-            Cache::forever('verify_jobs_diff', 0);
-            VerifyIndex::dispatch($max, $min, $query_chunk, $searchable_chunk);
         } else {
             Log::warning('Not able to obtain the highest or lowest ID for the day: ' . $date->format('Y-m-d'));
             Log::warning('Will try the fall back query.');
             $min = Statement::query()->selectRaw('min(id) as min')->where('created_at', '>=', $date->format('Y-m-d') . ' 00:00:00')->first()->min;
             $max = Statement::query()->selectRaw('max(id) as max')->where('created_at', '<=', $date->format('Y-m-d') . ' 23:59:59')->first()->max;
-            Cache::forever('verify_jobs', 1);
-            Cache::forever('verify_jobs_run', 1);
-            Cache::forever('verify_jobs_diff', 0);
-            VerifyIndex::dispatch($max, $min, $query_chunk, $searchable_chunk);
+            if ( ! $min || ! $max) {
+                Log::warning('Not able to obtain the highest or lowest ID for the day even with the fallback query: ' . $date->format('Y-m-d'));
+                return;
+            }
         }
+        Cache::forever('verify_jobs', 1);
+        Cache::forever('verify_jobs_run', 1);
+        Cache::forever('verify_jobs_diff', 0);
+        VerifyIndex::dispatch($max, $min, $query_chunk, $searchable_chunk);
     }
 }
