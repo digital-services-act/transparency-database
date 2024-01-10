81e3b0fc8c5a97970533484c73f69c43e2a75943
Alain Van Driessche <alainvd@gmail.com>
2024-01-10 15:43:28 +0100

    send back data

--- app/Console/Commands/StatementsIndexLastX.php
+++ app/Console/Commands/StatementsIndexLastX.php
@@ -34,8 +34,6 @@ class StatementsIndexLastX extends Command
 
         $statement_ids = Statement::query()->select(['id'])->where('created_at', '>=', $start)->pluck('id');
 
-        //Log::info('Indexing: ' .  $statement_ids->count());
-
         $statement_ids->chunk(600)->each(function($bag){
             StatementIndexBag::dispatch($bag->toArray());
         });
