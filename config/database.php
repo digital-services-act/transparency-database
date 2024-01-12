160b720a2e6e874a5afc151c112fd158a40b2a9c
Alain Van Driessche <alainvd@gmail.com>
2024-01-12 10:35:08 +0100

    docker image try

--- config/database.php
+++ config/database.php
@@ -44,19 +44,9 @@ return [
         ],
 
         'mysql' => [
-            'sticky' => true,
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'read' => [
-                'host' => [
-                    env('DB_HOST_READER', env('DB_HOST', '127.0.0.1')),
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    env('DB_HOST', '127.0.0.1'),
-                ],
-            ],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -72,8 +62,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
-
 
         'pgsql' => [
             'driver' => 'pgsql',
