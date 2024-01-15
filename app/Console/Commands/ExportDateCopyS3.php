5e86557dd65cf55ae4bd46b02db350a5cfb5376a
Robert Brown <robert.brown@contentcoffee.com>
2024-01-15 13:25:51 +0100

    make a copying command

---
+++ app/Console/Commands/ExportDateCopyS3.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\StatementCsvExportCat;
+use App\Jobs\StatementCsvExportCopyS3;
+use App\Services\DayArchiveService;
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class ExportDateCopyS3 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'exportcsv:copys3 {date=yesterday}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Concatenate the individual csv files to one.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(DayArchiveService $day_archive_service): void
+    {
+        $date = $this->argument('date');
+
+        if ($date === 'yesterday') {
+            $date = Carbon::yesterday();
+        } else {
+            try {
+                $date = Carbon::createFromFormat('Y-m-d', $date);
+            } catch (Exception $e) {
+                $this->error('Issue with the date provided, checked the format yyyy-mm-dd');
+                return;
+            }
+        }
+
+        $date_string = $date->format('Y-m-d');
+        $exports = $day_archive_service->buildBasicArray();
+        $versions = ['full', 'light'];
+        foreach ($exports as $export) {
+            foreach ($versions as $version) {
+                $zip = 'sor-' . $version . '-' . $export['slug'] . '-' . $date_string . '.csv.zip';
+                $sha1 = 'sor-' . $version . '-' . $export['slug'] . '-' . $date_string . '.csv.zip.sha1';
+                StatementCsvExportCopyS3::dispatch($zip, $sha1);
+            }
+        }
+    }
+}
---
+++ app/Jobs/StatementCsvExportCopyS3.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\DayArchiveService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use RuntimeException;
+use ZipArchive;
+
+class StatementCsvExportCopyS3 implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $zip;
+    public string $sha1;
+
+    public function __construct(string $zip, string $sha1)
+    {
+        $this->zip = $zip;
+        $this->sha1 = $sha1;
+    }
+
+    public function handle(): void
+    {
+        $path = Storage::path('s3ds');
+        shell_exec('cp ' . $this->zip . ' ' . $path);
+        shell_exec('cp ' . $this->sha1 . ' ' . $path);
+    }
+}
