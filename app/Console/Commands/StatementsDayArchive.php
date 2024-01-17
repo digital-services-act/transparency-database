fdb1280e5c4628521e441b4a3ab3a685fa3febba
Robert Brown <robert.brown@contentcoffee.com>
2024-01-17 17:14:22 +0100

    final

--- app/Console/Commands/StatementsDayArchive.php
+++ app/Console/Commands/StatementsDayArchive.php
@@ -128,22 +128,20 @@ class StatementsDayArchive extends Command
         $luggage = compact('date_string', 'start_jobs', 'finish_jobs', 'archive_jobs', 'csv_export_jobs', 'sha1_jobs', 'copys3_jobs', 'zip_jobs', 'reduce_jobs');
 
         Log::info('Day Archiving Started for: ' . $date_string . ' at ' . Carbon::now()->format('Y-m-d H:i:s'));
-        //@shell_exec('rm ' . Storage::path('') . '*' . $date_string . '*');
-//        Bus::batch($luggage['csv_export_jobs'])->finally(function() use($luggage) {
-//            Bus::batch($luggage['reduce_jobs'])->finally(function() use($luggage) {
-
-//                Bus::batch($luggage['zip_jobs'])->finally(function() use($luggage) {
-//                    Bus::batch($luggage['sha1_jobs'])->finally(function() use($luggage) {
-//                        Bus::batch($luggage['copys3_jobs'])->onQueue('s3copy')->finally(function() use($luggage) {
+        @shell_exec('rm ' . Storage::path('') . '*' . $date_string . '*');
+        Bus::batch($luggage['csv_export_jobs'])->finally(function() use($luggage) {
+            Bus::batch($luggage['reduce_jobs'])->finally(function() use($luggage) {
+                Bus::batch($luggage['zip_jobs'])->finally(function() use($luggage) {
+                    Bus::batch($luggage['sha1_jobs'])->finally(function() use($luggage) {
+                        Bus::batch($luggage['copys3_jobs'])->onQueue('s3copy')->finally(function() use($luggage) {
                             Bus::batch($luggage['archive_jobs'])->finally(function() use($luggage) {
-                                //@shell_exec('rm ' . Storage::path('') . '*' . $luggage['date_string'] . '*');
+                                @shell_exec('rm ' . Storage::path('') . '*' . $luggage['date_string'] . '*');
                                 Log::info('Day Archiving Ended for: ' . $luggage['date_string'] . ' at ' . Carbon::now()->format('Y-m-d H:i:s'));
                             })->dispatch();
-//                        })->dispatch();
-//                    })->dispatch();
-//                })->dispatch();
-
-//            })->dispatch();
-//        })->dispatch();
+                        })->dispatch();
+                    })->dispatch();
+                })->dispatch();
+            })->dispatch();
+        })->dispatch();
     }
 }
