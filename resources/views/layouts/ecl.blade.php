6807d2f201a1438e28543eeaac0d98ab3c9389f5
Robert Brown <robert.brown@contentcoffee.com>
2024-01-31 14:18:42 +0100

    on head is better than two.

--- resources/views/layouts/ecl.blade.php
+++ resources/views/layouts/ecl.blade.php
@@ -2,69 +2,69 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <head>
 
-        <title>@yield('title', 'Home') - DSA Transparency Database</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
-        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-        <link
+
+    <title>@yield('title', 'Home') - DSA Transparency Database</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link
             rel="stylesheet"
             href="{{ asset('static/ecl/styles/optional/ecl-ec-default.css') }}"
             crossorigin="anonymous"
             media="screen"
-        />
-        <link
+    />
+    <link
             rel="stylesheet"
             href="{{ asset('static/ecl/styles/optional/ecl-reset.css') }}"
             crossorigin="anonymous"
             media="screen"
-        />
-        <link
+    />
+    <link
             rel="stylesheet"
             href="{{ asset('static/ecl/styles/ecl-ec.css') }}"
             crossorigin="anonymous"
             media="screen"
-        />
-        <link
+    />
+    <link
             rel="stylesheet"
             href="{{ asset('static/ecl/styles/optional/ecl-ec-default-print.css') }}"
             crossorigin="anonymous"
             media="print"
-        />
-        <link
+    />
+    <link
             rel="stylesheet"
             href="{{ asset('static/ecl/styles/ecl-ec-print.css') }}"
             crossorigin="anonymous"
             media="print"
-        />
-        <style>
-            .scroll-to-top {
-                position: fixed;
-                bottom: 20px;
-                right: 20px;
-                background-color: #4a4e54; /* Change the background color to your preference */
-                color: #ffffff; /* Change the text color to your preference */
-                border-radius: 50%;
-                width: 60px;
-                height: 60px;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                cursor: pointer;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Optional: Add a box shadow for a subtle effect */
-            }
+    />
+    <style>
+        .scroll-to-top {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            background-color: #4a4e54; /* Change the background color to your preference */
+            color: #ffffff; /* Change the text color to your preference */
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Optional: Add a box shadow for a subtle effect */
+        }
 
-            /* Optional: Add hover effect */
-            .scroll-to-top:hover {
-                background-color: #0056b3; /* Change the background color on hover */
-            }
-        </style>
-        <script defer src="https://europa.eu/webtools/load.js?theme=ec" type="text/javascript"></script>
+        /* Optional: Add hover effect */
+        .scroll-to-top:hover {
+            background-color: #0056b3; /* Change the background color on hover */
+        }
+    </style>
+    <script defer src="https://europa.eu/webtools/load.js?theme=ec" type="text/javascript"></script>
 
-        @section('extra-head')
-        @show
-    </head>
+    @section('extra-head')
+    @show
+</head>
 <body class="ecl">
 <div id="root" style="padding-top: 0 !important;">
 
@@ -116,20 +116,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
         integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
         crossorigin="anonymous"></script>
-<script
-    src="{{ asset('static/ecl/scripts/ecl-ec.js') }}"
-    crossorigin="anonymous"
-></script>
+<script src="{{ asset('static/ecl/scripts/ecl-ec.js') }}"
+        crossorigin="anonymous"></script>
 <script>
     @if($ecl_init)
-    ECL.autoInit();
+    ECL.autoInit()
     @endif
 
-    function scrollToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+    function scrollToTop () {
+      window.scrollTo({
+        top: 0, behavior: 'smooth',
+      })
     }
 </script>
 @if(config('dsa.SITEID', false) && config('dsa.SITEPATH', false))
