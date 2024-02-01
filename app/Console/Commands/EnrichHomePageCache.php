a2b4a65da3b037ff995b091b391c06727a0747db
Robert Brown <robert.brown@contentcoffee.com>
2024-02-01 09:33:01 +0100

    making the cache building better.

--- app/Console/Commands/EnrichHomePageCache.php
+++ app/Console/Commands/EnrichHomePageCache.php
@@ -14,7 +14,7 @@ class EnrichHomePageCache extends Command
      *
      * @var string
      */
-    protected $signature = 'enrich-home-page-cache';
+    protected $signature = 'enrich-home-page-cache {--invalidate}';
 
     /**
      * The console command description.
@@ -26,8 +26,19 @@ class EnrichHomePageCache extends Command
     /**
      * Execute the console command.
      */
-    public function handle(StatementSearchService $statement_search_service)
+    public function handle(StatementSearchService $statement_search_service): void
     {
+        if ($this->option('invalidate')) {
+            Cache::delete('platforms_total');
+            Cache::delete('grand_total');
+            Cache::delete('top_categories');
+            Cache::delete('top_decisions_visibility');
+            Cache::delete('automated_decisions_percentage');
+            $this->info('Invalidating home page cache');
+        }
+
+        $this->info('Building home page cache');
+        // Now run and reset them if needed.
         $statement_search_service->grandTotal();
         Cache::remember('platforms_total', 60*60*24, function() {
             return max(1, Platform::nonDsa()->count());
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,13 +7,14 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    private const DAILY_AFTER_MIDNIGHT = '00:15';
+    private const DAILY_AFTER_MIDNIGHT = '00:10';
+    private const DAILY_SEVEN_AM = '07:00';
 
     protected function schedule(Schedule $schedule): void
     {
         // The main indexer
         $schedule->command('statements:index-date')->dailyAt(self::DAILY_AFTER_MIDNIGHT);
-        $schedule->command('enrich-home-page-cache')->hourly();
+        $schedule->command('enrich-home-page-cache --invalidate')->dailyAt(self::DAILY_SEVEN_AM);
     }
 
 
