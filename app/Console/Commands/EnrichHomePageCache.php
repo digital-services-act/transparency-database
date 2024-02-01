811d9f1c97128e2fe3a729aeacd6d5cf29e35c4a
Robert Brown <robert.brown@contentcoffee.com>
2024-02-01 10:02:38 +0100

    adding options for building and invalidating the cache.

--- app/Console/Commands/EnrichHomePageCache.php
+++ app/Console/Commands/EnrichHomePageCache.php
@@ -14,7 +14,7 @@ class EnrichHomePageCache extends Command
      *
      * @var string
      */
-    protected $signature = 'enrich-home-page-cache {--invalidate}';
+    protected $signature = 'enrich-home-page-cache {--invalidate} {--nobuild}';
 
     /**
      * The console command description.
@@ -28,23 +28,27 @@ class EnrichHomePageCache extends Command
      */
     public function handle(StatementSearchService $statement_search_service): void
     {
+        $day = 60 * 60 * 24;
+
         if ($this->option('invalidate')) {
+            $this->info('Invalidating home page cache');
             Cache::delete('platforms_total');
             Cache::delete('grand_total');
             Cache::delete('top_categories');
             Cache::delete('top_decisions_visibility');
             Cache::delete('automated_decisions_percentage');
-            $this->info('Invalidating home page cache');
         }
 
-        $this->info('Building home page cache');
-        // Now run and reset them if needed.
-        $statement_search_service->grandTotal();
-        Cache::remember('platforms_total', 60*60*24, function() {
-            return max(1, Platform::nonDsa()->count());
-        });
-        $statement_search_service->topCategories();
-        $statement_search_service->topDecisionVisibilities();
-        $statement_search_service->fullyAutomatedDecisionPercentage();
+        if (!$this->option('nobuild')) {
+            $this->info('Building home page cache');
+            // Now run and reset them if needed.
+            $statement_search_service->grandTotal();
+            Cache::remember('platforms_total', $day, function () {
+                return max(1, Platform::nonDsa()->count());
+            });
+            $statement_search_service->topCategories();
+            $statement_search_service->topDecisionVisibilities();
+            $statement_search_service->fullyAutomatedDecisionPercentage();
+        }
     }
 }
