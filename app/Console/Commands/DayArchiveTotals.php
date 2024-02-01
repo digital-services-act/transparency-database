9d6e701271ee4ad82de965c663d94d7f3abd26f6
Robert Brown <robert.brown@contentcoffee.com>
2024-02-01 09:20:25 +0100

    add a command to resave the day totals for the archive.

---
+++ app/Console/Commands/DayArchiveTotals.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\StatementCsvExportArchive;
+use App\Models\DayArchive;
+use App\Services\DayArchiveService;
+use App\Services\StatementSearchService;
+use Illuminate\Console\Command;
+
+class DayArchiveTotals extends Command
+{
+    use CommandTrait;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dayarchive:totals {date=yesterday} {--nosave}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create archives entries for platforms and exports.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(StatementSearchService $statement_search_service): void
+    {
+        $date = $this->sanitizeDateArgument();
+        $day_archives = DayArchive::query()->whereDate('date', $date)->get();
+
+        foreach ($day_archives as $day_archive) {
+
+            $total = $day_archive->platform ? $statement_search_service->totalForPlatformDate($day_archive->platform, $day_archive->date) : $statement_search_service->totalForDate($day_archive->date);
+            $this->info($day_archive->date . ' :: ' . ($day_archive->platform->name ?? 'Global')  . ' :: ' . $total);
+            $day_archive->total = $total;
+            if (!$this->option('nosave')) {
+                $day_archive->save();
+            }
+        }
+    }
+}
