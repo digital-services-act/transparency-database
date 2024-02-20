6f299c2afabaaa2b11f840880ff0f0f83b6caece
Alain Van Driessche <alainvd@gmail.com>
2024-02-20 16:31:41 +0100

    Show roles as mandatory

--- resources/views/components/user-form.blade.php
+++ resources/views/components/user-form.blade.php
@@ -1,21 +1,25 @@
 @props(['user' => null, 'options' => null, 'roles' => [], 'action' => 'create'])
 
 @if($action === 'create')
-<x-ecl.textfield label="Email" type="email" name="email" id="email" required=true value="{{ $user->email }}" />
+    <x-ecl.textfield label="Email" type="email" name="email" id="email" required=true value="{{ $user->email }}"/>
+@else
+    <x-ecl.textfield readonly="true" label="Email" type="email" name="email" id="email" required=true
+                     value="{{ $user->email }}"/>
 @endif
 
-<h3 class="ecl-u-type-heading-3 ecl-u-mb-l">Platform</h3>
+{{--<h3 class="ecl-u-type-heading-3 ecl-u-mb-l">Platform</h3>--}}
 
 <x-ecl.select label="Platform"
               name="platform_id"
               id="platform_id" default="{{ $user->platform_id }}"
               :options="$options['platforms']"
-              :required="false" />
+              :required="false"/>
 
-<h3 class="ecl-u-type-heading-3 ecl-u-mb-l">Roles</h3>
+{{--<h3 class="ecl-u-type-heading-3 ecl-u-mb-l">Roles</h3>--}}
+<x-ecl.label label="Roles" for="roles" name="roles" required="true"  />
 
 <ul class="ecl-unordered-list ecl-unordered-list--no-bullet ecl-u-mb-l">
-    <x-ecl.error-feedback name="roles" />
+    <x-ecl.error-feedback name="roles"/>
     @foreach($options['roles'] as $role)
         <li class="ecl-unordered-list__item">
             <x-ecl.checkbox id="permission-{{ $role->id }}"
