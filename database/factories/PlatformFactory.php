9a2aaa68e117adb10481c60f7713b8e695784bea
Alain Van Driessche <alainvd@gmail.com>
2024-02-21 17:56:32 +0100

    Merge with Dev

--- database/factories/PlatformFactory.php
+++ database/factories/PlatformFactory.php
@@ -6,43 +6,23 @@ use App\Models\Platform;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class PlatformFactory extends Factory
 {
 
-    private $platform_names = [
-        'Alibaba AliExpress',
-        'Amazon Store',
-        'Apple AppStore',
-        'Booking.com',
-        'Facebook',
-        'Google Play',
-        'Google Maps',
-        'Google Shopping',
-        'Instagram',
-        'LinkedIn',
-        'Pinterest',
-        'Snapchat',
-        'TikTok',
-        'Twitter',
-        'Wikipedia',
-        'YouTube',
-        'Zalando',
-        'Bing',
-        'Google Search',
-        'Platform 1',
-        'Platform 2',
-        'Platform 3',
-        'Platform 4',
-        'Platform 5',
-        'Platform 6',
-        'Platform 7',
-        'Platform 8',
-        'Platform 9',
-        'Platform 10'
-    ];
+
+
+
+
+    private function generatePlatformNames() {
+
+    }
+
+
+
 
     /**
      * Define the model's default state.
@@ -51,8 +31,13 @@ class PlatformFactory extends Factory
      */
     public function definition()
     {
+        $platform_names = [];
+        for ($i = 1; $i <= 50; $i++) {
+            $platform_names[] = 'Platform ' . $i;
+        }
+
         return [
-            'name' => $this->faker->unique()->randomElement($this->platform_names),
+            'name' => $this->faker->unique()->randomElement($platform_names),
             'dsa_common_id' => $this->faker->uuid(),
         ];
     }
