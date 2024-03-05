d2c012aeea9090f905ac933f6346102933fa452a
Alain Van Driessche <alainvd@gmail.com>
2024-03-05 15:21:22 +0100

    Multiple Statements - Check for duplicates

--- resources/views/layouts/ecl.blade.php
+++ resources/views/layouts/ecl.blade.php
@@ -130,7 +130,7 @@
     }
 </script>
 @if(config('dsa.SITEID', false) && config('dsa.SITEPATH', false))
-{{ // DO NOT SPLIT THIS LINE }}
+{{--DO NOT SPLIT THIS LINE--}}
 <script type="application/json">{"utility":"analytics","siteID":"{{ config('dsa.SITEID') }}","sitePath":["{{ config('dsa.SITEPATH') }}"],"instance":"ec"}</script>
 @endif
 <script type="application/json">{"utility": "cck","url": "{{ route('page.show', ['page' => 'cookie-policy']) }}"}
