1879ea21280dfae5b7f693b7ed73a18740a1dcfe
Robert Brown <robert.brown@contentcoffee.com>
2024-03-13 14:40:06 +0100

    keep it on the correct queue

--- app/Jobs/StatementDeDuplicateRange.php
+++ app/Jobs/StatementDeDuplicateRange.php
@@ -43,7 +43,7 @@ class StatementDeDuplicateRange implements ShouldQueue
         if ($end < $this->max) {
             $next_start = $this->min + $this->chunk + 1;
             // Start the next one.
-            self::dispatch($next_start, $this->chunk, $this->max);
+            self::dispatch($next_start, $this->chunk, $this->max)->onQueue('dedupe');
         }
 
         $range = range($this->min, $end);
