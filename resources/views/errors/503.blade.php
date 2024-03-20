de9c3c5161c3e32f819fa5dfdb34b3749b86b7bc
Alain Van Driessche <alainvd@gmail.com>
2024-03-20 13:02:56 +0100

    503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>503 - Service Unavailable - DSA Transparency Database</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link
+        rel="stylesheet"
+        href="{{ asset('static/ecl/styles/optional/ecl-ec-default.css') }}"
+        crossorigin="anonymous"
+        media="screen"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('static/ecl/styles/optional/ecl-reset.css') }}"
+        crossorigin="anonymous"
+        media="screen"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('static/ecl/styles/ecl-ec.css') }}"
+        crossorigin="anonymous"
+        media="screen"
+    >
+    <style>
+
+        #root {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        .content-wrap {
+            flex-grow: 1;
+        }
+
+        .ecl-site-footer {
+            flex-shrink: 0;
+        }
+    </style>
+
+    @section('extra-head')
+    @show
+</head>
+<body class="ecl">
+<div id="root" style="padding-top: 0 !important;">
+
+    <header class="ecl-site-header" data-ecl-auto-init="SiteHeader">
+        <div class="ecl-site-header__header">
+            <div class="ecl-site-header__container ecl-container">
+                <div class="ecl-site-header__top">
+                    <a href="{{ route('home') }}" class="ecl-link ecl-link--standalone ecl-site-header__logo-link"
+                       aria-label="European Commission">
+                        <img alt="European Commission logo"
+                             title="European Commission"
+                             class="ecl-site-header__logo-image ecl-site-header__logo-image-desktop"
+                             src="{{asset('static/ecl/images/logo/positive/logo-ec--en.svg')}}">
+                    </a>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="ecl-site-header__banner">
+            <div class="ecl-container">
+                <div class="ecl-site-header__site-name">DSA Transparency Database</div>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="ecl-container ecl-u-mb-xl content-wrap">
+        <div class="ecl-row">
+            <div class="ecl-col-12">
+                <div class="ecl-container">
+                    <main class="ecl-u-pb-xl" id="main-content">
+                        <div class="ecl-row ecl-u-mb-xl-4xl">&nbsp;</div>
+
+                        <div class="ecl-container">
+                            <div class="ecl-row">
+                                <div class="ecl-col-s-12">
+                                    <div id="block-ewcms-theme-main-page-content" class="ecl-u-mb-l">
+                                        <div>
+                                            <h2 id="nf-heading" class="ecl-u-type-heading-2">We&rsquo;ll be back soon!</h2>
+                                            <div class="ecl">
+                                                <p id="nf-text" class="ecl-u-type-paragraph">Sorry for the inconvenience, but we&rsquo;re performing some maintenance to improve your experience.</p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ecl-row ecl-u-mb-xl-4xl">&nbsp;</div>
+                        <div class="ecl-row ecl-u-mb-xl-4xl">&nbsp;</div>
+                        <div class="ecl-row ecl-u-mb-xl-4xl">&nbsp;</div>
+
+                    </main>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <footer class="ecl-site-footer">
+        <div class="ecl-container ecl-site-footer__container">
+            <div class="ecl-site-footer__row">
+                <div class="ecl-site-footer__column">
+                    <div class="ecl-site-footer__section">
+                        <a href="https://ec.europa.eu"
+                           class="ecl-link ecl-link--standalone ecl-site-footer__logo-link" aria-label="European Commission">
+                            <img alt="European Commission logo" title="European Commission" class="ecl-site-footer__logo-image-desktop"
+                                 src="{{asset('static/ecl/images/logo/negative/logo-ec--en.svg')}}">
+                        </a>
+                        <div class="ecl-site-footer__description">
+                            This site is managed by the Directorate-General for &quot;Communications Networks, Content and Technology&quot;
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
