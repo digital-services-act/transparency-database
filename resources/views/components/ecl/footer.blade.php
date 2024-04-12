3ea3348996c535383a6b0e2e4cb62800c4649eec
Alain Van Driessche <alainvd@gmail.com>
2024-04-12 08:20:12 +0200

    Footer and Profile

--- resources/views/components/ecl/footer.blade.php
+++ resources/views/components/ecl/footer.blade.php
@@ -11,8 +11,7 @@
                              src="{{asset('static/ecl/images/logo/negative/logo-ec--en.svg')}}">
                     </a>
                     <div class="ecl-site-footer__description">
-                        This site is managed by the Directorate-General for &quot;Communications Networks, Content and
-                        Technology&quot;
+                        {{__('footer.This site is managed by the Directorate-General for "Communications Networks, Content and Technology"')}}
                     </div>
                 </div>
 
@@ -20,16 +19,16 @@
             <div class="ecl-site-footer__column">
                 <div class="ecl-site-footer__section">
                     <h2 class="ecl-site-footer__title ecl-site-footer__title--separator">
-                        Contact us </h2>
+                        {{__('footer.Contact us')}} </h2>
                     <ul class="ecl-site-footer__list">
                         <li class="ecl-site-footer__list-item">
                             <a href="https://digital-strategy.ec.europa.eu/en/write-us"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Link to Contact information of the DG">Contact information</a></li>
+                               aria-label="Link to Contact information of the DG">{{__('footer.Contact information')}}</a></li>
                         <li class="ecl-site-footer__list-item">
                             <a href="{{route('feedback.index')}}"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Link to Contact information of the DG">Feedback</a>
+                               aria-label="Link to Contact information of the DG">{{__('footer.Feedback')}}</a>
                         </li>
                     </ul>
                 </div>
@@ -37,43 +36,43 @@
             <div class="ecl-site-footer__column">
                 <div class="ecl-site-footer__section">
                     <h2 class="ecl-site-footer__title ecl-site-footer__title--separator">
-                        About us </h2>
+                        {{__('footer.About us')}} </h2>
                     <ul class="ecl-site-footer__list">
                         <li class="ecl-site-footer__list-item">
                             <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX%3A32022R2065&qid=1671203215141"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Link to Information about the DG">Digital Services Act Regulation</a></li>
+                               aria-label="Link to Information about the DG">{{__('footer.Digital Services Act Regulation')}}</a></li>
 
                         <li class="ecl-site-footer__list-item">
                             <a href="https://digital-strategy.ec.europa.eu/en/policies/safer-online"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Learn more about the DSA">Learn more about the DSA</a></li>
+                               aria-label="Learn more about the DSA">{{__('footer.Learn more about the DSA')}}</a></li>
 
                         <li class="ecl-site-footer__list-item">
                             <a href="https://digital-strategy.ec.europa.eu/en/policies/safer-online"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Learn more about the DSA">Announcements</a></li>
+                               aria-label="Learn more about the DSA">{{__('footer.Announcements')}}</a></li>
 
 
                     </ul>
                 </div>
                 <div class="ecl-site-footer__section">
                     <h2 class="ecl-site-footer__title ecl-site-footer__title--separator">
-                        Policies </h2>
+                        {{__('footer.Policies')}} </h2>
                     <ul class="ecl-site-footer__list">
                         <li class="ecl-site-footer__list-item">
                             <a href="{{ route('page.show', ['page' => 'privacy-policy']) }}"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Cookie Policy">Privacy Policy</a></li>
+                               aria-label="Cookie Policy">{{__('footer.Privacy Policy')}}</a></li>
                         <li class="ecl-site-footer__list-item">
                             <a href="{{ route('page.show', ['page' => 'legal-information']) }}"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Cookie Policy">Legal Notice</a></li>
+                               aria-label="Cookie Policy">{{__('footer.Legal Notice')}}</a></li>
 
                         <li class="ecl-site-footer__list-item">
                             <a href="{{ route('page.show', ['page' => 'data-retention-policy']) }}"
                                class="ecl-link ecl-link--standalone ecl-link--inverted ecl-site-footer__link"
-                               aria-label="Cookie Policy">Data Retention Policy</a></li>
+                               aria-label="Cookie Policy">{{__('footer.Data Retention Policy')}}</a></li>
 
                     </ul>
                 </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
 
-    <h1 class="ecl-u-type-heading-1">User Profile </h1>
+    <h1 class="ecl-u-type-heading-1">{{__('profile.User Profile')}} </h1>
     <p class="ecl-u-type-paragraph"
        style="font-size:16pc; margin-top:-26px; font-style: italic !important">{{auth()->user()->email}}</p>
 
@@ -18,78 +18,73 @@
     <div class="ecl-row ecl-u-mb-l">
         @can('create statements')
             <div class="ecl-col-3">
-                <a class="ecl-button ecl-button--primary" href="{{ route('profile.api.index') }}">API Token
-                    Management</a>
+                <a class="ecl-button ecl-button--primary" href="{{ route('profile.api.index') }}">{{__('profile.API Token Management')}}</a>
             </div>
         @endcan
         <div class="ecl-col-3">
-            <a class="ecl-button ecl-button--primary" href="/logout">Logout</a>
+            <a class="ecl-button ecl-button--primary" href="/logout">{{__('profile.Logout')}}</a>
         </div>
     </div>
 
     @canany(['create users','create platforms','view logs','view platforms',])
 
-        <h2 class="ecl-u-type-heading-2">Administration</h2>
+        <h2 class="ecl-u-type-heading-2">{{__('profile.Administration')}}</h2>
 
         <div class="ecl-row ecl-u-mb-l">
             @can('create users')
                 <div class="ecl-col-3">
-                    <a class="ecl-button ecl-button--primary" href="{{ route('user.index') }}">Manage Users</a>
+                    <a class="ecl-button ecl-button--primary" href="{{ route('user.index') }}">{{__('profile.Manage Users')}}</a>
                 </div>
             @endcan
 
             @can('create platforms')
                 <div class="ecl-col-3">
-                    <a class="ecl-button ecl-button--primary" href="{{ route('platform.index') }}">Manage Platforms</a>
+                    <a class="ecl-button ecl-button--primary" href="{{ route('platform.index') }}">{{__('profile.Manage Platforms')}}</a>
                 </div>
             @endcan
 
             @can('view logs')
                 <div class="ecl-col-3">
-                    <a class="ecl-button ecl-button--primary" href="{{ route('log-messages.index') }}">Log Messages</a>
+                    <a class="ecl-button ecl-button--primary" href="{{ route('log-messages.index') }}">{{__('profile.Log Messages')}}</a>
                 </div>
             @endcan
 
             @can('view platforms')
                 <div class="ecl-col-3">
-                    <a class="ecl-button ecl-button--primary" href="{{ route('onboarding.index') }}">Onboarding
-                        Dashboard</a>
+                    <a class="ecl-button ecl-button--primary" href="{{ route('onboarding.index') }}">{{__('profile.Onboarding Dashboard')}}</a>
                 </div>
             @endcan
         </div>
     @endcanany
 
 
-    <h2 class="ecl-u-type-heading-2">Assistance</h2>
+    <h2 class="ecl-u-type-heading-2">{{__('profile.Assistance')}}</h2>
     <div class="ecl-row ecl-u-mb-l">
         <div class="ecl-col-12">
             <p class="ecl-u-type-paragraph">
-                For any type of issues please contact:
+            {{__('profile.For any type of issues please contact:')}}
             <pre>
                 CNECT-DSA-HELPDESK&#64;ec.europa.eu
         </pre>
             </p>
 
             <p class="ecl-u-type-paragraph" style="max-width:none !important">
-                <strong>If you are an online platform</strong> and wish to set up your statement of reasons submission
-                process:<br/><br/>
+                 {!! __('profile.If you are an online platform and wish to set up your statement of reasons submission process:') !!}
+                <br/><br/>
 
-                Please register your intent to comply <a
+                {{__('profile.Please register your intent to comply')}} <a
                     href="https://ec.europa.eu/eusurvey/runner/DSA-ComplianceStamentsReasons">https://ec.europa.eu/eusurvey/runner/DSA-ComplianceStamentsReasons</a><br/><br/>
 
-                Later on you will be contacted by the DSC of the country of establishment of the online platform (you
-                will find the already appointed DSCs here <a
+                {{__('profile.Later on you will be contacted by the DSC of the country of establishment of the online platform (you will find the already appointed DSCs here')}}
+                 <a
                     href="https://digital-strategy.ec.europa.eu/en/policies/dsa-cooperation">https://digital-strategy.ec.europa.eu/en/policies/dsa-cooperation</a>).
-                This is the first step required to be onboarded as an online platform with obligations under Article
-                24(5) of the DSA. Please note that Digital Services Coordinators should be appointed by 17th February
-                2024 at the latest.<br/><br/>
 
-                Currently, only designated VLOPs (Verified List of Platforms) have access to the API (both sandbox/test
-                and production) for submissions of statements of reasons under the DSA (Digital Services Act). Other
-                platforms that have notified the Commission will be contacted by their Digital Service Coordinator for
-                onboarding.<br/><br/>
+                {{__('profile.This is the first step required to be onboarded as an online platform with obligations under Article 24(5) of the DSA. Please note that Digital Services Coordinators should be appointed by 17th February 2024 at the latest.')}}<br/><br/>
 
-                Thank you
+                {{__('profile.Currently, only designated VLOPs (Verified List of Platforms) have access to the API (both sandbox/test and production) for submissions of statements of reasons under the DSA (Digital Services Act). Other platforms that have notified the Commission will be contacted by their Digital Service Coordinator for onboarding.')}}
+                <br/><br/>
+
+                {{__('profile.Thank you')}}
 
             </p>
         </div>
