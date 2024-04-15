3df50a7295204d58bdc379789200c85a04fee2ec
Alain Van Driessche <alainvd@gmail.com>
2024-04-15 14:57:56 +0200

    Dashboard layout IT

--- lang/it/dashboard.php
+++ lang/it/dashboard.php
@@ -2,16 +2,17 @@
 
 return [
 
-    "The dashboard below provides a user-friendly and interactive interface to explore summarized data, offering a comprehensive overview. Start exploring the data by clicking on different elements. You can navigate across the following pages, from left to right at the bottom of the dashboard:" => "Il cruscotto sottostante fornisce un'interfaccia utente intuitiva e interattiva per esplorare dati riassuntivi, offrendo una panoramica completa. Inizia ad esplorare i dati cliccando su diversi elementi. Puoi navigare attraverso le seguenti pagine, da sinistra a destra in fondo al cruscotto:",
+    "The dashboard below provides a user-friendly and interactive interface to explore summarized data, offering a comprehensive overview. Start exploring the data by clicking on different elements. You can navigate across the following pages, from left to right at the bottom of the dashboard:" => "La Dashboard qui sotto fornisce un'interfaccia utente amichevole e interattiva per esplorare dati sintetizzati, offrendo una panoramica completa. Inizia a esplorare i dati cliccando su diversi elementi. Puoi navigare attraverso le seguenti pagine, da sinistra a destra nella parte inferiore della Dashboard:",
     "Overview" => "Panoramica",
-    "Timelines" => "Cronologie",
+    "Timelines" => "Timeline",
     "Violations" => "Violazioni",
     "Restrictions" => "Restrizioni",
     "Other analysis" => "Altre analisi",
-    "For additional guidance on making the best use of the tool, please refer to instructions below the dashboard." => "Per ulteriori indicazioni su come utilizzare al meglio lo strumento, consulta le istruzioni sotto il cruscotto.",
+    "For additional guidance on making the best use of the tool, please refer to instructions below the dashboard." => "Per ulteriori indicazioni su come utilizzare al meglio lo strumento, fare riferimento alle istruzioni sotto la dashboard.",
     "Instructions" => "Istruzioni",
 
     "Introduction" => "Introduzione",
+
 
 
 ];
