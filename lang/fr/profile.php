aedf04e17eae566621ce8c326decd8cb93acb0a7
Alain Van Driessche <alainvd@gmail.com>
2024-07-15 10:11:25 +0200

    User Profile page: remove the onboarding text

--- lang/fr/profile.php
+++ lang/fr/profile.php
@@ -10,11 +10,5 @@ return [
     "Log Messages" => "Messages de journal",
     "Onboarding Dashboard" => "Tableau de bord d'intégration",
     "Assistance" => "Assistance",
-    "For any type of issues please contact:" => "Pour tout type de problèmes, veuillez contacter :",
-    "If you are an online platform and wish to set up your statement of reasons submission process:" => "<strong>Si vous êtes une plateforme en ligne</strong> et souhaitez mettre en place votre processus de soumission des motifs de votre déclaration :",
-    "Later on you will be contacted by the DSC of the country of establishment of the online platform (you will find the already appointed DSCs here" => "Plus tard, vous serez contacté par le DSC du pays d'établissement de la plateforme en ligne (vous trouverez ici les DSC déjà nommés",
-    "This is the first step required to be onboarded as an online platform with obligations under Article 24(5) of the DSA. Please note that Digital Services Coordinators should be appointed by 17th February 2024 at the latest." => "Ceci est la première étape requise pour être intégré en tant que plateforme en ligne avec des obligations en vertu de l'article 24(5) de la DSA. Veuillez noter que les coordinateurs des services numériques doivent être nommés au plus tard le 17 février 2024.",
-    "Please register your intent to comply" => "Veuillez enregistrer votre intention de vous conformer",
-    "Currently, only designated VLOPs (Verified List of Platforms) have access to the API (both sandbox/test and production) for submissions of statements of reasons under the DSA (Digital Services Act). Other platforms that have notified the Commission will be contacted by their Digital Service Coordinator for onboarding." => "Actuellement, seules les VLOP désignées (liste vérifiée des plateformes) ont accès à l'API (sandbox/test et production) pour les soumissions de motifs de leur déclaration dans le cadre de la DSA (loi sur les services numériques). Les autres plateformes ayant informé la Commission seront contactées par leur coordinateur de service numérique pour l'intégration.",
-    "Thank you" => "Merci"
+    "For any type of issues please contact:" => "Pour tout type de problèmes, veuillez contacter :"
 ];
