c157daa6a8967760d8729039aa7cd68c38f71c91
Robert Brown <robert.brown@contentcoffee.com>
2024-10-09 08:59:59 +0200

    index the statements at the moment of the call base on before and after ids

--- app/Console/Commands/StatementsIndexRangeSeq.php
+++ app/Console/Commands/StatementsIndexRangeSeq.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Jobs\StatementSearchableChunk;
+use App\Services\StatementSearchService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -28,15 +29,22 @@ class StatementsIndexRangeSeq extends Command
     /**
      * Execute the console command.
      */
-    public function handle(): void
+    public function handle(StatementSearchService $statement_search_service): void
     {
 
         $chunk = $this->intifyArgument('chunk');
-        $min = $this->argument('min') === 'default' ? DB::table('statements')->selectRaw('MIN(id) AS min')->first()->min : (int)$this->argument('min');
+
+        if ($this->argument('min') === 'opensearch') {
+            $min = $statement_search_service->highestId();
+        } else {
+            $min = $this->argument('min') === 'default' ? DB::table('statements')->selectRaw('MIN(id) AS min')->first()->min : (int)$this->argument('min');
+        }
+
         $max = $this->argument('max') === 'default' ? DB::table('statements')->selectRaw('MAX(id) AS max')->first()->max : (int)$this->argument('max');
 
-        Log::info('Indexing started for range: ' . $min . ' :: ' . $max . ' at ' . Carbon::now()->format('Y-m-d H:i:s'));
-
-        StatementSearchableChunk::dispatch($min, $max, $chunk);
+        if ($min && $max && $min < $max) {
+            Log::info('Indexing started for range: ' . $min . ' :: ' . $max . ' at ' . Carbon::now()->format('Y-m-d H:i:s'));
+            StatementSearchableChunk::dispatch($min, $max, $chunk);
+        }
     }
 }
