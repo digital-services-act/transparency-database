51947313bfaa1e063292fa8602a035dcd73b83ca
Robert Brown <robert.brown@ext.ec.europa.eu>
2024-11-20 21:39:24 +0100

    Start making a factory for day archives

--- app/Models/DayArchive.php
+++ app/Models/DayArchive.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class DayArchive extends Model
 {
+    use HasFactory;
     protected $guarded = [];
 
     protected $casts = [
--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
