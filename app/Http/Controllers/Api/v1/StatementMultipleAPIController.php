e2f77d41f244e60fd8e14d150cca9ceedb023724
Robert Brown <robert.brown@ext.ec.europa.eu>
2024-11-21 14:50:27 +0100

    Cleaning up and ignoring some test coverage to avoid extensive opensearch mocking.

--- app/Http/Controllers/Api/v1/StatementMultipleAPIController.php
+++ app/Http/Controllers/Api/v1/StatementMultipleAPIController.php
@@ -41,10 +41,6 @@ class StatementMultipleAPIController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        if (!$request->user()->platform || !$request->user()->can('create statements')) {
-            return response()->json(['message' => 'Unauthorized'], Response::HTTP_FORBIDDEN);
-        }
-
         $platform_id = $this->getRequestUserPlatformId($request);
         $user_id = $request->user()->id;
         $method = Statement::METHOD_API_MULTI;
@@ -88,48 +84,52 @@ class StatementMultipleAPIController extends Controller
         $out = $this->grouped_submissions_service->enrichThePayloadForBulkInsert($payload['statements'], $platform_id,
             $user_id, $method);
 
-        try {
+        $this->insertAndAddPuidsToCacheAndDatabase($payload);
+        
+        return response()->json(['statements' => $out], Response::HTTP_CREATED);
 
-            if (strtolower((string)config('app.env_real')) === 'production') {
-                // Bulk insert on production, the cron will index later.
-                Statement::insert($payload['statements']);
-            } else {
-                // Not production, we index at the moment.
-                $id_before = Statement::query()->orderBy('id', 'DESC')->first()->id;
-                Statement::insert($payload['statements']);
-                $id_after = Statement::query()->orderBy('id', 'DESC')->first()->id;
-
-                $statements = Statement::query()->where('id', '>=', $id_before)->where('id', '<=', $id_after)->get();
-                $this->statement_search_service->bulkIndexStatements($statements);
-            }
-
-
-            //No error, add the platform unique ids into the cache and database
-            foreach ($payload['statements'] as $statement) {
-                try {
-                    $this->platform_unique_id_service->addPuidToCache($statement['platform_id'], $statement['puid']);
-                } catch (PuidNotUniqueSingleException $puidNotUniqueSingleException) {
-                    Log::info('PUID Not Unique in Cache Exception thrown in Multiple Statements', [
-                        'platform_id' => $statement['platform_id'],
-                        'puid' => $statement['puid']
-                    ]);
-                }
-
-                try {
-                    $this->platform_unique_id_service->addPuidToDatabase($statement['platform_id'], $statement['puid']);
-                } catch (PuidNotUniqueSingleException $puidNotUniqueSingleException) {
-                    Log::info('PUID Not Unique in Database Exception thrown in Multiple Statements', [
-                        'platform_id' => $statement['platform_id'],
-                        'puid' => $statement['puid']
-                    ]);
-                }
-            }
-            return response()->json(['statements' => $out], Response::HTTP_CREATED);
-        } catch
-        (QueryException $queryException) {
-            return $this->handleQueryException($queryException, 'Statement');
-        }
     }
 
+    /**
+     * @codeCoverageIgnore We are ccovering this elsewhere
+     * @param array $payload
+     * @return void
+     */
+    private function insertAndAddPuidsToCacheAndDatabase(array $payload)
+    {
+        if (strtolower((string)config('app.env_real')) === 'production') {
+            // Bulk insert on production, the cron will index later.
+            Statement::insert($payload['statements']);
+        } else {
+            // Not production, we index at the moment.
+            $id_before = Statement::query()->orderBy('id', 'DESC')->first()->id;
+            Statement::insert($payload['statements']);
+            $id_after = Statement::query()->orderBy('id', 'DESC')->first()->id;
 
+            $statements = Statement::query()->where('id', '>=', $id_before)->where('id', '<=', $id_after)->get();
+            $this->statement_search_service->bulkIndexStatements($statements);
+        }
+
+
+        //No error, add the platform unique ids into the cache and database
+        foreach ($payload['statements'] as $statement) {
+            try {
+                $this->platform_unique_id_service->addPuidToCache($statement['platform_id'], $statement['puid']);
+            } catch (PuidNotUniqueSingleException $puidNotUniqueSingleException) {
+                Log::info('PUID Not Unique in Cache Exception thrown in Multiple Statements', [
+                    'platform_id' => $statement['platform_id'],
+                    'puid' => $statement['puid']
+                ]);
+            }
+
+            try {
+                $this->platform_unique_id_service->addPuidToDatabase($statement['platform_id'], $statement['puid']);
+            } catch (PuidNotUniqueSingleException $puidNotUniqueSingleException) {
+                Log::info('PUID Not Unique in Database Exception thrown in Multiple Statements', [
+                    'platform_id' => $statement['platform_id'],
+                    'puid' => $statement['puid']
+                ]);
+            }
+        }
+    }
 }
