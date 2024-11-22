1a790a6e7700ecade426f416143b4915cc3950ac
Robert Brown <robert.brown@ext.ec.europa.eu>
2024-11-22 12:43:46 +0100

    improving the toggle command and laying the foundation to test more commands.

---
+++ app/Console/Commands/Traits/DocumentableTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\Traits;
+
+trait DocumentableTrait
+{
+    /**
+     * Get the command description with examples.
+     *
+     * @return string
+     */
+    public function getDetailedDescription(): string
+    {
+        $description = $this->description . "\n\n";
+        $description .= "Usage:\n";
+        $description .= "  " . $this->signature . "\n\n";
+        
+        if (property_exists($this, 'arguments') && !empty($this->arguments)) {
+            $description .= "Arguments:\n";
+            foreach ($this->arguments as $argument => $desc) {
+                $description .= "  {$argument}: {$desc}\n";
+            }
+            $description .= "\n";
+        }
+        
+        if (property_exists($this, 'examples') && !empty($this->examples)) {
+            $description .= "Examples:\n";
+            foreach ($this->examples as $example) {
+                $description .= "  {$example}\n";
+            }
+        }
+        
+        return $description;
+    }
+}
---
+++ tests/Feature/Console/Commands/Traits/TraitTestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Console\Commands\Traits;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\CreatesApplication;
+
+abstract class TraitTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        // Only create application, skip database setup
+        $this->createApplication();
+    }
+}
