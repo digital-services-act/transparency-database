e5b6c45821c16196c08d9ba9766cdd992c21e2e1
Robert Brown <robert.brown@ext.ec.europa.eu>
2025-02-14 16:22:06 +0100

    california

--- resources/views/statement/index.blade.php
+++ resources/views/statement/index.blade.php
@@ -14,7 +14,7 @@
     <div class="ecl-row ecl-u-mt-l ecl-u-mb-xl">
         <div class="ecl-col-l-12">
 
-            <div class="ecl-u-type-paragraph" style="max-width:none !important">
+            <div class="ecl-u-type-paragraph ecl-u-mb-l" style="max-width:none !important">
                 On this page, you can search for individual statements of reasons submitted by providers of online
                 platforms.<br />
                 <br />
@@ -34,22 +34,32 @@
                 If you are interested in a programmatic search instead, you can our Search API.
             </div>
 
-            <x-ecl.accordion label="How Search works">
-                You can use the free-text search to look for specific words within the free text fields of each statement of
-                reasons.
-                Searching for the platform name in the free-text search only returns statements of reasons which contain the
-                platform
-                name entered in one of their free-text fields. To search for statements of reasons submitted by a specific
-                online
-                platform, please use the “Platform” dropdown selector to select one or more platforms of interest
-                instead.<br />
-                <br />
-                To filter statements of reasons by (a combination of) other variables, in addition to the platform they were
-                submitted
-                by, the type of restriction(s) imposed, their information source and their category, please click on
-                “Advanced Filter”
-                to expand the additional search filters.
-            </x-ecl.accordion>
+
+            <x-ecl.expandable label="How Search works">
+                <p class="ecl-u-type-paragraph">
+                    You can use the free-text search to look for specific words within the free text fields of each
+                    statement of
+                    reasons.
+                    Searching for the platform name in the free-text search only returns statements of reasons which
+                    contain
+                    the
+                    platform
+                    name entered in one of their free-text fields. To search for statements of reasons submitted by a
+                    specific
+                    online
+                    platform, please use the “Platform” dropdown selector to select one or more platforms of interest
+                    instead.<br />
+                    <br />
+                    To filter statements of reasons by (a combination of) other variables, in addition to the platform
+                    they
+                    were
+                    submitted
+                    by, the type of restriction(s) imposed, their information source and their category, please click on
+                    “Advanced Filter”
+                    to expand the additional search filters.
+                </p>
+            </x-ecl.expandable>
+
 
         </div>
     </div>
@@ -58,9 +68,8 @@
 
     <x-statement.search-form :similarity_results="$similarity_results" :options="$options" />
 
-
-
-    <div class="ecl-u-mt-l ecl-u-d-flex ecl-u-justify-content-between ecl-u-mt-l">
+    <div class="ecl-u-mt-xl">&nbsp;<!-- CALIFORNIA --></div>
+    <div class="ecl-u-d-flex ecl-u-justify-content-between ecl-u-mt-xl">
 
         <div class="item ecl-u-type-paragraph">
             @if (!$reindexing)
