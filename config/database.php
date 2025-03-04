aa11025d345ae3b44b22558d5b166cc89a0ea0d0
Alain Van Driessche <alainvd@gmail.com>
2025-03-04 14:02:33 +0100

    Remove SSL

--- config/database.php
+++ config/database.php
@@ -44,8 +44,19 @@ return [
         ],
 
         'mysql' => [
+            'sticky' => true,
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'read' => [
+                'host' => [
+                    env('DB_HOST_READER', env('DB_HOST', '127.0.0.1')),
+                ],
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_HOST', '127.0.0.1'),
+                ],
+            ],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -54,15 +65,12 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'sslmode' => 'require',
-            'options'   => array(
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                PDO::MYSQL_ATTR_SSL_KEY => '/certs/client-key.pem',
-                PDO::MYSQL_ATTR_SSL_CERT => '/certs/client-cert.pem',
-                PDO::MYSQL_ATTR_SSL_CA => '/certs/ca.pem',
-            ),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mysqlpreprod' => [
