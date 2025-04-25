6172ee9d80c0ad20af7319415036a23b695a7991
Alain Van Driessche <alainvd@gmail.com>
2025-04-25 16:44:03 +0200

    Remove TLS

--- config/database.php
+++ config/database.php
@@ -44,26 +44,16 @@ return [
         ],
 
         'mysql' => [
-            'sticky' => true,
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'read' => [
-                'host' => [
-                    env('DB_HOST_READER', env('DB_HOST', '127.0.0.1')),
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    env('DB_HOST', '127.0.0.1'),
-                ],
-            ],
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
