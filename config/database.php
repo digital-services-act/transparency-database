18af445a5a6802b4cc1d2d3f17de450ad4ba6a29
Alain Van Driessche <alainvd@gmail.com>
2025-04-25 16:27:58 +0200

    Remove TLS

--- config/database.php
+++ config/database.php
@@ -44,19 +44,8 @@ return [
         ],
 
         'mysql' => [
-            'sticky' => true,
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'read' => [
-                'host' => [
-                    env('DB_HOST_READER', env('DB_HOST', '127.0.0.1')),
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    env('DB_HOST', '127.0.0.1'),
-                ],
-            ],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -65,12 +54,13 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'sslmode' => 'require',
+            'options'   => array(
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::MYSQL_ATTR_SSL_CA => resource_path('certificates/ca.pem'),
+            ),
         ],
 
         'mysqlpreprod' => [
