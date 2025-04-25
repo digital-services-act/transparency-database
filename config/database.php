36a2eaf651447b197daafd1fc2bfc1b7038a20cf
Alain Van Driessche <alainvd@gmail.com>
2025-04-25 16:41:04 +0200

    Remove TLS

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
