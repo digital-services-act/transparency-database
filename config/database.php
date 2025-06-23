3fa2c014797a557cfec0aaf4dea16cdf2123c280
Robert Brown <robert.brown@ext.ec.europa.eu>
2025-06-23 14:04:30 +0200

    start the process of trying out postgresql.

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,12 +60,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('POSTGRESQL_ADDON_HOST', '127.0.0.1'),
+            'port' => env('POSTGRESQL_ADDON_PORT', '5432'),
+            'database' => env('POSTGRESQL_ADDON_DB', 'forge'),
+            'username' => env('POSTGRESQL_ADDON_USER', 'forge'),
+            'password' => env('POSTGRESQL_ADDON_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
