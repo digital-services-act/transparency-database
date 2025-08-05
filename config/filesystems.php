8d0dc4b611e31f33d5447586b2e8124b98032ac4
Robert Brown <brownrl@gmail.com>
2025-08-05 11:04:08 +0200

    get the s3 to work.

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,13 +58,13 @@ return [
 
         's3ds' => [
             'driver' => 's3',
-            'key' => env('AWS_DS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_DS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_DS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('CELLAR_ADDON_KEY_ID'),
+            'secret' => env('CELLAR_ADDON_KEY_SECRET'),
+            'region' => env('CELLAR_ADDON_DEFAULT_REGION', 'eu-central-1'),
+            'bucket' => env('CELLAR_ADDON_DS_BUCKET', 'dsa-sor-data-dumps'),
+            'url' => 'https://' . env('CELLAR_ADDON_HOST', ''),
+            'endpoint' => env('CELLAR_ADDON_ENDPOINT', ''),
+            'use_path_style_endpoint' => env('CELLAR_ADDON_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'visibility' => 'public'
         ],
