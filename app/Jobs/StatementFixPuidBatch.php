f2d95d44368ce13206a3e90c5393a8f34356d5ac
Bogdan Calin <calin.s.bogdan@gmail.com>
2025-09-15 17:32:14 +0300

    Discord cleanup: seeder, commands, job, migration for faulty_ids

---
+++ app/Jobs/StatementFixPuidBatch.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Statement;
+use App\Models\StatementAlpha;
+use App\Services\StatementSearchService;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class StatementFixPuidBatch implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $batchNr;
+    protected $ids;
+    protected $table;
+
+    public function __construct(string $table, array $ids, int $batchNr)
+    {
+        $this->batchNr = $batchNr;
+        $this->ids = $ids;
+        $this->table = $table;
+    }
+
+    public function handle(StatementSearchService $opensearch)
+    {
+        $start = Carbon::now();
+        $idsStr = implode(',', $this->ids);
+
+        // Update DB
+        $query = <<<SQL
+            UPDATE {$this->table}
+            SET puid = SUBSTRING_INDEX(puid, '-', 1)
+            WHERE id in ({$idsStr})
+        SQL;
+
+        DB::unprepared($query);
+
+        // Reload updated statements for OpenSearch
+        $updatedStatements = $this->table === 'statements'
+            ? StatementAlpha::whereIn('id', $this->ids)->get()->makeVisible('puid')
+            : Statement::whereIn('id', $this->ids)->get()->makeVisible('puid');
+
+        $opensearch->bulkIndexStatements($updatedStatements, true);
+
+        // Mark in faulty_ids
+        DB::table('faulty_ids')
+            ->whereIn('id', $this->ids)
+            ->update([
+                'updated_db_at' => now(),
+                'updated_os_at' => now(),
+            ]);
+
+        Log::info("Finished batch {$this->batchNr} for {$this->table}. Duration: " . Carbon::now()->diffForHumans($start));
+    }
+}
