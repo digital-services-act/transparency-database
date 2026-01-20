841541d152c4e891b7adf055dc661c1bf1283af7
Bogdan Calin <calin.s.bogdan@gmail.com>
2026-01-20 14:17:51 +0200

    PUID Cache locking for the Multi API endpoint

--- app/Services/PlatformUniqueIdService.php
+++ app/Services/PlatformUniqueIdService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Exceptions\PuidNotUniqueMultipleException;
 use App\Exceptions\PuidNotUniqueSingleException;
 use App\Models\PlatformPuid;
+use App\Models\Statement;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
@@ -12,7 +13,11 @@ class PlatformUniqueIdService
 {
     public $lock_valid_seconds = 30;
 
-    public function __construct(protected int $cache_valid_days = 2) {}
+    public function __construct(
+        protected GroupedSubmissionsService $groupService,
+        protected StatementSearchService $opensearch,
+        protected int $cache_valid_days = 2
+    ) {}
 
     public function handlePuid(mixed $puid, int $platform_id): void
     {
@@ -23,7 +28,7 @@ class PlatformUniqueIdService
 
         // @codeCoverageIgnoreStart
         if (! $lock->get()) {
-            Log::info('Lock encountered for PUID ' . $puid . ' on platform ' . $platform_id);
+            // Log::info('Lock encountered for PUID ' . $puid . ' on platform ' . $platform_id . ' (single)');
             throw new PuidNotUniqueSingleException($puid);
         }
         // @codeCoverageIgnoreEnd
@@ -34,6 +39,88 @@ class PlatformUniqueIdService
         } finally {
             optional($lock)->release();
         }
+    }
+
+    public function handleBatchPayload(array &$payload): array
+    {
+        $output = [];
+        $puids = array_map(
+            static fn($potential_statement) => $potential_statement['puid'],
+            $payload['statements']
+        );
+
+        // Check if PUIDs are unique in the Request made by the client
+        $this->checkDuplicatesInRequest($puids);
+
+        $locks = [];
+        $failedLocks = [];
+
+        try {
+            foreach ($puids as $puid) {
+                $key = $this->getCacheKey($payload['platform_id'], $puid);
+                $lockKey = "lock:puid:{{$key}}";
+
+                $lock = Cache::lock($lockKey, $this->lock_valid_seconds);
+
+                if (! $lock->get()) {
+                    // @codeCoverageIgnoreStart
+                    $failedLocks[] = $puid;
+                    // @codeCoverageIgnoreEnd
+                } else {
+                    $locks[$puid] = $lock;
+                }
+            }
+
+            // Locks encountered => duplicates, abort
+            // @codeCoverageIgnoreStart
+            if (! empty($failedLocks)) {
+                Log::info('Locks encountered for PUIDs ' . implode(', ', $failedLocks) . ' on platform ' . $payload['platform_id'] . ' (multi)');
+                throw new PuidNotUniqueMultipleException($failedLocks);
+            }
+            // @codeCoverageIgnoreEnd
+
+            // Check cache and database for duplicates
+            $this->checkDuplicatesInCache($puids, $payload['platform_id']);
+            $this->checkDuplicatesInPlatformPuids($puids, $payload['platform_id']);
+
+            // No duplicates, add all PUIDs to cache and database
+            foreach ($puids as $puid) {
+                $this->addPuidToCache($payload['platform_id'], $puid);
+                $this->addPuidToDatabase($payload['platform_id'], $puid);
+            }
+
+            $statements = $payload['statements'];
+            // "Enrich" the payload with additional data
+            $output = $this->groupService->enrichThePayloadForBulkInsert(
+                $statements,
+                $payload['platform_id'],
+                $payload['user_id'],
+                $payload['method']
+            );
+
+            // Now we save the statements
+            // @codeCoverageIgnoreStart
+            if (env('APP_ENV_REAL') === 'production') {
+                // Bulk insert on production, the cron will index later.
+                Statement::insert($statements);
+                // @codeCoverageIgnoreEnd
+            } else {
+                // Not production, we index at the moment.
+                $id_before = Statement::query()->orderBy('id', 'DESC')->first()->id;
+
+                Statement::insert($statements);
+                $id_after = Statement::query()->orderBy('id', 'DESC')->first()->id;
+
+                $statements = Statement::query()->where('id', '>=', $id_before)->where('id', '<=', $id_after)->get();
+                $this->opensearch->bulkIndexStatements($statements);
+            }
+        } finally {
+            foreach ($locks as $lock) {
+                optional($lock)->release();
+            }
+        }
+
+        return $output;
     }
 
     public function getCacheKey(int $platform_id, mixed $puid): string
