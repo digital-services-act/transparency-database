e983f2a561cc327b2419310e8e548eac3e672b27
Angélique VD <angeliquevd@users.noreply.github.com>
2026-02-20 18:47:19 +0100

    Remove datetime from custom log formatter configuration

--- app/Logging/PrependAppName.php
+++ app/Logging/PrependAppName.php
@@ -12,10 +12,8 @@ class PrependAppName
         $appName = config('app.name', 'Laravel');
 
         foreach ($logger->getHandlers() as $handler) {
-            // On impose un format où le nom d'app est AVANT le datetime
             $handler->setFormatter(new LineFormatter(
-                format: "({$appName}) [%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
-                dateFormat: 'Y-m-d H:i:s',
+                format: "({$appName}) %channel%.%level_name%: %message% %context% %extra%",
                 allowInlineLineBreaks: true,
                 ignoreEmptyContextAndExtra: true,
             ));
