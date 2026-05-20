b0435d236b4d0b43e7a218d31c9d8f316eab162b
Robert Brown <robert.brown@ext.ec.europa.eu>
2026-05-20 09:37:54 +0200

    separating the uri variable out in elastic again. The elastic php library needs both.

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -1,6 +1,8 @@
 <?php
 
 $host = env('ES_ADDON_HOST');
+$username = env('ES_ADDON_USER', null);
+$password = env('ES_ADDON_PASSWORD', null);
 $scheme = env('ES_ADDON_SCHEME', 'https');
 
 if (is_string($host)) {
@@ -9,16 +11,29 @@ if (is_string($host)) {
 
 if (! is_string($host) || $host === '') {
     $host = null;
-} elseif (! preg_match('#^https?://#i', $host)) {
-    $host = rtrim((string) $scheme, ':/').'://'.$host;
+} elseif (preg_match('#^(https?)://(.+)$#i', $host, $matches)) {
+    $scheme = $matches[1];
+    $host = $matches[2];
+}
+
+$uri = null;
+
+if ($host !== null) {
+    $scheme = rtrim((string) $scheme, ':/');
+
+    if (is_string($username) && $username !== '' && is_string($password) && $password !== '') {
+        $uri = $scheme.'://'.rawurlencode($username).':'.rawurlencode($password).'@'.$host;
+    } else {
+        $uri = $scheme.'://'.$host;
+    }
 }
 
 return [
     'hosts' => [$host],
-    'uri' => [$host],
+    'uri' => [$uri],
     'basicAuthentication' => [
-        'username' => env('ES_ADDON_USER', null),
-        'password' => env('ES_ADDON_PASSWORD', null),
+        'username' => $username,
+        'password' => $password,
     ],
     'apiKey' => env('ES_ADDON_API_KEY', null),
     'retries' => env('ES_ADDON_RETRIES', 2),
