36a107fbac90b97a7418ad5fe5d514e6a91afb49
Angélique Van Driessche <angevd76@gmail.com>
2026-06-09 15:22:17 +0200

    Uncomment migration logic for `database_velocities` table

--- database/migrations/2026_06_03_000000_tune_autovacuum_for_statement_delete_tables.php
+++ database/migrations/2026_06_03_000000_tune_autovacuum_for_statement_delete_tables.php
@@ -12,39 +12,39 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (DB::getDriverName() !== 'pgsql') {
-            return;
-        }
-
-        foreach (['statements_beta', 'platform_puids'] as $table) {
-            DB::statement(sprintf(
-                'ALTER TABLE %s SET (
-                    autovacuum_vacuum_scale_factor = 0.001,
-                    autovacuum_vacuum_threshold = 50000,
-                    autovacuum_analyze_scale_factor = 0.001,
-                    autovacuum_analyze_threshold = 50000
-                )',
-                $table
-            ));
-        }
+//        if (DB::getDriverName() !== 'pgsql') {
+//            return;
+//        }
+//
+//        foreach (['statements_beta', 'platform_puids'] as $table) {
+//            DB::statement(sprintf(
+//                'ALTER TABLE %s SET (
+//                    autovacuum_vacuum_scale_factor = 0.001,
+//                    autovacuum_vacuum_threshold = 50000,
+//                    autovacuum_analyze_scale_factor = 0.001,
+//                    autovacuum_analyze_threshold = 50000
+//                )',
+//                $table
+//            ));
+//        }
     }
 
     public function down(): void
     {
-        if (DB::getDriverName() !== 'pgsql') {
-            return;
-        }
-
-        foreach (['statements_beta', 'platform_puids'] as $table) {
-            DB::statement(sprintf(
-                'ALTER TABLE %s RESET (
-                    autovacuum_vacuum_scale_factor,
-                    autovacuum_vacuum_threshold,
-                    autovacuum_analyze_scale_factor,
-                    autovacuum_analyze_threshold
-                )',
-                $table
-            ));
-        }
+//        if (DB::getDriverName() !== 'pgsql') {
+//            return;
+//        }
+//
+//        foreach (['statements_beta', 'platform_puids'] as $table) {
+//            DB::statement(sprintf(
+//                'ALTER TABLE %s RESET (
+//                    autovacuum_vacuum_scale_factor,
+//                    autovacuum_vacuum_threshold,
+//                    autovacuum_analyze_scale_factor,
+//                    autovacuum_analyze_threshold
+//                )',
+//                $table
+//            ));
+//        }
     }
 };
