dad22d211e936bf56821d579b75a3dd7443da307
Angélique Van Driessche <angevd76@gmail.com>
2026-06-09 15:21:14 +0200

    Uncomment migration logic for `database_velocities` table

--- database/migrations/2026_02_13_112037_create_database_velocities_table.php
+++ database/migrations/2026_02_13_112037_create_database_velocities_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-//        Schema::create('database_velocities', function (Blueprint $table) {
-//            $table->id();
-//            $table->unsignedBigInteger('max_statement_id');
-//            $table->decimal('rows_per_second', 10, 2);
-//            $table->timestamps();
-//        });
+        Schema::create('database_velocities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('max_statement_id');
+            $table->decimal('rows_per_second', 10, 2);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-//        Schema::dropIfExists('database_velocities');
+        Schema::dropIfExists('database_velocities');
     }
 };
